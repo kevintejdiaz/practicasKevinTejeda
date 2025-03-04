d709318043f28c41aca8125fbdf4afedf7a97142
kevintejdiaz <145122698+kevintejdiaz@users.noreply.github.com>
2025-03-04 16:34:46 +0100

    Add files via upload

--- M3Programacio/UF5/AC5/Program.cs
+++ M3Programacio/UF5/AC5/Program.cs
@@ -12,11 +12,14 @@
             Console.WriteLine("Seleccione una opción:");
             Console.WriteLine("1. Agregar Producto");
             Console.WriteLine("2. Mostrar Productos");
-            Console.WriteLine("3. Agregar Cliente");
-            Console.WriteLine("4. Mostrar Clientes");
-            Console.WriteLine("5. Agregar Empleado");
-            Console.WriteLine("6. Mostrar Empleados");
-            Console.WriteLine("7. Salir");
+            Console.WriteLine("3. Eliminar Producto");
+            Console.WriteLine("4. Agregar Cliente");
+            Console.WriteLine("5. Mostrar Clientes");
+            Console.WriteLine("6. Eliminar Cliente");
+            Console.WriteLine("7. Agregar Empleado");
+            Console.WriteLine("8. Mostrar Empleados");
+            Console.WriteLine("9. Eliminar Empleado");
+            Console.WriteLine("10. Salir");
             string opcion = Console.ReadLine();
 
             switch (opcion)
@@ -34,16 +37,26 @@
                     apiProductos.MostrarElementos();
                     break;
                 case "3":
+                    Console.Write("Ingrese ID del Producto a eliminar: ");
+                    int idEliminarP = int.Parse(Console.ReadLine());
+                    apiProductos.EliminarElemento(idEliminarP);
+                    break;
+                case "4":
                     Console.Write("Ingrese ID: ");
                     int idC = int.Parse(Console.ReadLine());
                     Console.Write("Ingrese Nombre: ");
                     string nombreC = Console.ReadLine();
                     apiClientes.AgregarElemento(new Cliente { Id = idC, Nombre = nombreC });
                     break;
-                case "4":
+                case "5":
                     apiClientes.MostrarElementos();
                     break;
-                case "5":
+                case "6":
+                    Console.Write("Ingrese ID del Cliente a eliminar: ");
+                    int idEliminarC = int.Parse(Console.ReadLine());
+                    apiClientes.EliminarElemento(idEliminarC);
+                    break;
+                case "7":
                     Console.Write("Ingrese ID: ");
                     int idE = int.Parse(Console.ReadLine());
                     Console.Write("Ingrese Nombre: ");
@@ -52,10 +65,15 @@
                     string puesto = Console.ReadLine();
                     apiEmpleados.AgregarElemento(new Empleado { Id = idE, Nombre = nombreE, Puesto = puesto });
                     break;
-                case "6":
+                case "8":
                     apiEmpleados.MostrarElementos();
                     break;
-                case "7":
+                case "9":
+                    Console.Write("Ingrese ID del Empleado a eliminar: ");
+                    int idEliminarE = int.Parse(Console.ReadLine());
+                    apiEmpleados.EliminarElemento(idEliminarE);
+                    break;
+                case "10":
                     continuar = false;
                     break;
                 default:
